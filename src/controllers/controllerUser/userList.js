a12f63a3453b946bf71a602a9fe6033c150407c9
Túlio Koogi <tulio-koogi@hotmail.com>
2024-08-21 21:27:01 -0300

    configure db postgre

--- src/controllers/controllerUser/userList.js
+++ src/controllers/controllerUser/userList.js
@@ -1,4 +1,4 @@
-//import userModel from "../../models/userModel.js"
+import userModel from "../../models/userModel.js"
 
 const userList = async(req, res) => {
     const users = await userModel.getAll()
